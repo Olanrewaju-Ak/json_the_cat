ed93f3928bf62af05d6306268206ead76382a2d4
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-18 15:04:37 -0700

    edge cases taken care of

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,14 +5,22 @@ const args = process.argv.slice(2);
 // the search query prameter issued by the user in the CLI
 const parameter = args[0];
 
+//API call using dynamic parameter gotten from CLI
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${parameter}`;
 
 request(url, (error, response, body) => {
   console.log("statusCode: ", response && response.statusCode);
+
+  // to check if the breed entered by user doesnt exist
+
   if (error) {
-    console.log(error);
+    console.log("error: ", error);
   }
   const data = JSON.parse(body);
-  console.log(typeof data);
-  console.log(data[0].description);
+
+  if (data.length === 0) {
+    console.log("Breed not found");
+  } else {
+    console.log(data[0].description);
+  }
 });
