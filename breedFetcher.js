7f98dbdf801850a477539954cb9cb06d60905acb
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-18 19:46:22 -0700

    refactor breedfetcher code and export function as module

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,25 @@
 const request = require("request");
 
-const args = process.argv.slice(2);
+const fetchBreedDescription = function (breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-// the search query prameter issued by the user in the CLI
-const parameter = args[0];
+  request(url, (error, response, body) => {
+    // to check if the breed entered by user doesnt exist
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // convert body from string to object
+    const data = JSON.parse(body);
 
-//API call using dynamic parameter gotten from CLI
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${parameter}`;
+    //checking if the breedName doesnt exist
+    if (data.length === 0) {
+      callback("Breed not found.Check your spellings or enter another breed name", null);
+      return;
+    }
 
-request(url, (error, response, body) => {
-  console.log("statusCode: ", response && response.statusCode);
+    callback(null, data[0].description);
+  });
+};
 
-  // to check if the breed entered by user doesnt exist
-
-  if (error) {
-    console.log("error: ", error);
-  }
-  const data = JSON.parse(body);
-
-  if (data.length === 0) {
-    console.log("Breed not found");
-  } else {
-    console.log(data[0].description);
-  }
-});
+module.exports = { fetchBreedDescription };
