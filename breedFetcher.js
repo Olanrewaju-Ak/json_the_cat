df100b8fa9f821bba3308346cb8ca9c4287ec27b
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-18 20:02:23 -0700

    edit error message in callback function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,6 @@
 const request = require("request");
 
-const fetchBreedDescription = function (breedName, callback) {
+const fetchBreedDescription = function(breedName, callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
   request(url, (error, response, body) => {
